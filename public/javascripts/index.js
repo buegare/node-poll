fb9119c181a720d7e8747938b13c670f7bccdf4d
Bruno Buegare <brunobuegare@gmail.com>
2017-05-18 16:59:58 -0700

    Fix problem with the answers that weren't saved on the database

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,4 @@
 $('#add-answer').on('click', function(e){
 	e.preventDefault();
-	$('<input class="answer" name="a1" type="text" placeholder="Answer">').insertAfter($('.answer').last());
+	$('<input class="answer" name="answer" type="text" placeholder="Answer">').insertAfter($('.answer').last());
 });
