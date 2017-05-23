dc3ac71b48ff11d33b19f7275f3c0fc5c0de3cdb
Bruno Buegare <brunobuegare@gmail.com>
2017-05-22 18:12:07 -0700

    Implement checks before creating new poll and make nav brand a link

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,3 +2,24 @@ $('#add-answer').on('click', function(e){
 	e.preventDefault();
 	$('<input class="answer" name="answer" type="text" placeholder="Answer">').insertAfter($('.answer').last());
 });
+
+$('#create-poll-btn').on('click', function(e) {
+	var poll_title = $('#poll-title').val();
+	var answer = $('.answer').val();
+
+	if(poll_title === '') {
+		alert('You must enter a title for this Poll');
+		e.preventDefault();
+		return;
+	} 
+
+	$('.answer').each(function(i, v) { 
+		if($(this).val() === '') {
+			alert('You must enter an answer');
+			e.preventDefault();
+		}
+	});
+
+	
+
+});
